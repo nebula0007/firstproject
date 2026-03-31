a710c701c158188f9fd5e5106bb9cd6e53422551
nebula0007 <Ishantksingh135@gmail.com>
2026-03-31 07:31:55 +0000

    Day four

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -3,51 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// function App() {
+//   return(
+//     <>
+//     <button onClick="callfun()">ok</button>
+//     <h1> </h1>
+//     </>
+//   )
+// }
+//     <div>
+//       <h1>Welcome to new project</h1>
+//    </div>
+
 function App() {
-  const name="Ishant";
-  const greet="Hello Everyone";
-  let x=10;
-  let y=20;
-function fun()
-{
-  return "inside fun function";
-}
-function myname()
-{
-  return "I am Ishant";
-}
-
-function sum(a,b)
-{
-  return a+b;
-}
-
-function calc(a, b, op) {
-  if (op === '+') {
-    return a + b;
-  } else if (op === '-') {
-    return a - b;
-  } else {
-    return a * b;
+  function abc() {
+    alert("function Called");
   }
-}
-  return(
-    
+
+  function apple() {
+    alert("you have click on apple button");
+  }
+
+  function banana() {
+    alert("you have click on banana button");
+  }
+
+  return (
     <div>
-  <h1>project run</h1>
-  <h3>Value of variable {name}</h3>
-  <h3>Value of variable {greet}</h3>
-  <h3>10+20={x+y}</h3>
-  <h1>Use of function in jsx</h1>
-  <h3>example of simple function {fun()}</h3>
-  <h2>{myname()}</h2>
-  <h3>exam of paramiterize function {sum(60,90)}</h3>
-  <h3>{calc(7,8,"+")}</h3>
-  <h3>{calc(7,8,"-")}</h3>
-  <h3>{calc(7,8)}</h3>
-  </div>
-  )
+      <h1>Welcome to new project</h1>
+      
+      <button onClick={abc}>click</button>
+      <button onClick={apple}>Apple</button>
+      <button onClick={banana}>Banana</button>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
